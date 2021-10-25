a3a626868ab7691b5ffebad726d3b5892c7bf3b1
Umishi <88778094+Umishi@users.noreply.github.com>
2021-10-25 22:27:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ TOKEN = 'token here'
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    if message.channel == client.get_channel(channel_1 here):
+    if message.channel == client.get_channel(channel_1 here) and not message.author.bot :
         channel = client.get_channel(channel_2 here)
         await channel.send(message.content)
 
+    if message.channel == client.get_channel(channel_2 here) and not message.author.bot :
+        channel = client.get_channel(channel_1 here)
+        await channel.send(message.content)
 
 client.run(TOKEN)
